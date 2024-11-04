901ddb46085ad0afa9dc67fde43e057b00c2436f
RvC12345 <ravigimhan31@gmail.com>
2024-11-04 10:43:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ from text import Start,Help,Commands
 API_ID = os.environ.get("apiid")
 API_HASH = os.environ.get("apihash")
 BOT_TOKEN = os.environ.get("tk")
-AuthU=int(os.environ.get("auth"))
+#AuthU=int(os.environ.get("auth"))
+AuthU = list(map(int, os.environ.get("auth").split(',')))
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,workers=10,plugins=dict(root="plugin"))
 
