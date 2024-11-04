7a1e46baadccf4bc1c24e505712e82d91fad532b
RvC12345 <ravigimhan31@gmail.com>
2024-11-04 09:53:58 +0530

    Update command.py

--- plugin/command.py
+++ plugin/command.py
@@ -1,6 +1,20 @@
 from pyrogram import Client, filters
 from text import Start, Help, Commands
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
 
 @Client.on_message(filters.command("start") & filters.private,)
 async def start_bot(bot, m: Message):
-  bot.send_message(start)
+  #bot.send_message(start)
+  inline_keyboard=[
+  InlineKeyboardButton("HELP",callback_data="HELP"),
+  InlineKeyboardButton("COMMANDS",callback_data="Commands")
+    ]
+  reply_markup = InlineKeyboardMarkup(inline_keyboard)
+  await bot.send_message(
+         chat_id=m.chat.id,
+         text=Start,
+         reply_markup=reply_markup,
+         #reply_to_message_id=update.id
+          )
+
