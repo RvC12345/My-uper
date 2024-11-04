8ecd20d838e10f7df31be70d13b4b925dbdb1304
RvC12345 <ravigimhan31@gmail.com>
2024-11-04 09:29:29 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,3 @@
+Start="**Hi....<name>🫡\n\nI'am a Url Uploader bot.\nSend me a url to see the magic**"
+Help="**RX URL UPLOADER**\n\n This is an simple Url Uploader....\n🔰Made By: **RXBots**\n🔰Lang: **Python**\n🔰Server: **koyeb**\n🔰Owner: **RVX**"
+Commands="Now Available:\n\n/start- start the bot\n\n/help- For Help and Info"
