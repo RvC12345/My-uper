a1b1a534e7bd5ed8b227389870dc033dd95abdb4
RvC12345 <ravigimhan31@gmail.com>
2024-11-04 07:58:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,12 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Replace these with your own bot credentials
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = os.environ.get("apiid")
+API_HASH = os.environ.get("apihash")
+BOT_TOKEN = os.environ.get("tk")
+AuthU=int(os.environ.get("auth"))
 
-app = Client("url_uploader_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Progress bar function to create visual progress with 10 blocks
 def create_progress_bar(progress):
