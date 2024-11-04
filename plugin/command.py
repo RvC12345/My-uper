65a2c877534e619978a53be4e0670b233fdef697
RvC12345 <ravigimhan31@gmail.com>
2024-11-04 10:20:44 +0530

    Update command.py

--- plugin/command.py
+++ plugin/command.py
@@ -3,13 +3,16 @@ from text import Start, Help, Commands
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 
+def Btn(t,data=""):
+  return InlineKeyboardButton(t,callback_data=data)
+
 @Client.on_message(filters.command("start"))
 async def start_bot(bot, m):
   #bot.send_message(start)
   inline_keyboard=[
         [
-    InlineKeyboardButton("HELP",callback_data="HELP"),
-    InlineKeyboardButton("COMMANDS",callback_data="Commands")
+    InlineKeyboardButton("HELP",callback_data="help"),
+    InlineKeyboardButton("COMMANDS",callback_data="commands")
         ]
     ]
   reply_markup = InlineKeyboardMarkup(inline_keyboard)
@@ -20,3 +23,20 @@ async def start_bot(bot, m):
          #reply_to_message_id=update.id
           )
 
+@Client.on_callback_query()
+async def _clback(bot,update):
+  if update.data == "help":
+     update.message.edit(
+       text=Help,
+       reply_markup=InlineKeyboardMarkup([[Btn("Back",data="home")]])
+     )
+  elif update.data == "commands":
+    update.message.edit(
+       text=Commands,
+       reply_markup=InlineKeyboardMarkup([[Btn("Back",data="home")]])
+    )
+  elif update.data == "home":
+    update.message.edit(
+       text=Start,
+       reply_markup=InlineKeyboardMarkup([[Btn("HELP",data="help"),Btn("COMMANDS",data="commands")]])
+    )
